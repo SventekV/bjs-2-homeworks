0ed70dfaddcb31a13d9a32cab0c219d5813d8011
Vadim Sventek <sventek1980@yandex.ru>
2021-10-19 17:09:44 +0300

    solveEquation add

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  "use strict";
+  let x;
+  let x1;
+  let x2;
+  let d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+ x1 = (-b + Math.sqrt(d) ) / (2 * a); x2 = (-b - Math.sqrt(d) )/(2 * a);
+arr = [x1, x2]
+  }
+   else if (d === 0) { x = -b/(2*a) 
+arr = [x];
+   }
+   else if (d < 0) {
+   	arr = [];
+   }
+  
   return arr; // array
 }
 
