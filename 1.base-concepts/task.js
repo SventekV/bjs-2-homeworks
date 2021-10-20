ed24f2e5141da5d89c9cb41f658c05940eed0fcd
Vadim Sventek <sventek1980@yandex.ru>
2021-10-20 14:22:50 +0300

    FIRST dz

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,8 @@
+ "use strict";
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
-  "use strict";
+ 
   let x;
   let x1;
   let x2;
@@ -21,9 +22,21 @@ arr = [x];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  
+if (isNaN(percent)) {
+  return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
+} else if (isNaN(contribution)) {
+  return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`
+} else if (isNaN(amount)) {
+  return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`
 }
+const month = new Date().getMonth();
+const year = new Date().getFullYear();
+const numberOFyears = date.getFullYear() - year;
+const n = numberOFyears * 12 - month + date.getMonth();
+const P = (percent / 100) / 12;
+const S = amount - contribution;
+const payment = S * (P + (P / (((1 + P) ** n) - 1)))
+const totalAmount = (payment * n).toFixed(2);
+
+return Number(totalAmount);}
