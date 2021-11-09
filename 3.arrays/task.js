496bac95c450e0ee7708b1b0f3f4ed22e08b8ed6
Vadim Sventek <sventek1980@yandex.ru>
2021-11-09 23:13:10 +0300

    dz 3 complete

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
-function compareArrays(arr1, arr2) {
+
+ function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+result = (arr1.length == arr2.length) && arr1.every(function(element, index) {
+    return element === arr2[index]; 
+});
+return result
+                               }
 
-  return result; // boolean
-}
+
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((number) => number > 0)
+  .filter((number) => number % 3 == 0)
+  .map((number) => number * 10)
 
   return resultArr; // array
 }
