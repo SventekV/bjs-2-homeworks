444d2eba4be99f28d4e091c5e2cb907fe9ad6c3d
Vadim Sventek <sventek1980@yandex.ru>
2022-02-11 18:52:18 +0300

    заверщено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,35 +1,55 @@
+
 function parseCount(value) { 
-  value = Number.parseInt(value);
-   if (Number.isNaN(value) == true) {
-    const error = new Error("Невалидное значение");
-    throw error;}
-  else {return value}
-  }
-
- function validateCount(value){ 
- try {
-parseCount(value); const result = parseCount(value);
-return result
+  let a = Number.parseInt(value);
+   if (isNaN(a)) {
+    throw e = new Error("Невалидное значение"); 
+    }
+  else {return a;}
  }
-
  
-  
- catch (er) {
-  return er
-};
-
+function validateCount(value){ 
+ try {
+  let result = parseCount(value);
+  return result;
  }
+ catch (e) {
+   return e;
+ };
+}
 
- class Triangle {
 
-  constructor (a, b, c) {
+class Triangle {
+ constructor (a, b, c) {
 this.a = a;
 this.b = b;
 this.c = c;
-if ((a + b) < c) {const error = new Error("Невалидное значение");throw error;}
-  }
- getPerimeter() {const perimetr = this.a + this.b + this.c;
-    return perimetr; }
+if ((a + b) < c || (a + c) < b || (b + c) < a) {
+  const error = new Error("Треугольник с такими сторонами не существует");
+  throw error;}
+}
 
-    getArea() {const p = (1 / 2) * (triangle.getPerimeter()); const area = Number( Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3); ; return area }  
+getPerimeter() {
+  const perimetr = this.a + this.b + this.c;
+  return perimetr; 
+}
+
+getArea() {
+      const p = (1 / 2) * (this.a + this.b + this.c);
+      const area = +( Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3); ; return area 
+     }  
+}
+
+
+
+function getTriangle(a, b, c) {
+  try {
+   const triangle = new Triangle(a,b,c);
+   return triangle;
+  }
+ catch (error) {const object = {
+  getPerimeter() {return "Ошибка! Треугольник не существует"},
+  getArea() {return "Ошибка! Треугольник не существует" } 
+  };
+   return object; 
+ } 
 }
