9be29a3539ad7e96e9757d724012beba9d102b4f
Vadim Sventek <sventek1980@yandex.ru>
2022-03-03 21:56:35 +0300

    dz 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,29 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+function wrapper(...args) {
+    const hash = args.join(","); console.log(hash); 
+    let idx = cache.findIndex((item) => item["hash"] === hash); console.log(idx) 
+    if (idx !== -1 ) { 
+        console.log("Из кэша: " + cache[idx].value); 
+        return "Из кэша: " + cache[idx].value;
+    }
+
+    let result = func(...args); 
+  let obj = {
+      hash: hash,
+      value: result
+    } 
+    cache.push(obj) ; 
+    if (cache.length > 5) { 
+      cache.shift() 
+    }
+    console.log("Вычисляем: " + result);
+    console.log(obj);
+    console.log(cache)
+    return "Вычисляем: " + result;  
+}
+return wrapper;
 }
 
 
